c1f3413ea0a84a2cf99fbf8692865e2b4bab2b98
Wouter Glorieux <wouter@valyrian.tech>
2024-04-17 10:39:16 +0200

    no longer needed

--- openvoice_server/start.sh
+++ openvoice_server/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Activate conda environment
 source activate openvoice
-# Install libmagic
-conda install --yes libmagic
+
 # Start the server
 uvicorn openvoice_server:app --host "0.0.0.0" --port 8000
